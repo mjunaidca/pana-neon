86f55643d7162a1e09f186a564e3840bcb93a995
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-04-07 14:53:44 +0500

    Hello World With Neon

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    NEON_DATABASE_URL:
+      "postgres://mr.junaid.ca:Of8jtYhgeH4C@ep-red-thunder-428017.us-east-2.aws.neon.tech/neondb",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,48 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+// export async function GET(request: Request) {
+//   return new Response('Hello, Next.js!')
+// }
+
+import { NextRequest, NextResponse } from "next/server";
+
+import { Pool } from 'pg';
+
+import {
+  Kysely,
+  PostgresDialect,
+  Generated,
+  ColumnType,
+  Selectable,
+  Insertable,
+  Updateable,
+} from 'kysely';
+
+interface dev_Table {
+  id: Generated<number>;
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface Database {
+  dev: dev_Table
+}
+
+export async function GET(request: NextRequest) {
+  const db = new Kysely<Database>({
+    dialect: new PostgresDialect({
+      pool: new Pool({
+        ssl: true,
+        connectionString: process.env.NEON_DATABASE_URL
+      })
+    })
+  });
+
+  const result = await db
+    .selectFrom("dev")
+    .selectAll()
+    .execute();
+
+  console.log("Neon Dev DB Reult on API", result);
+  return new NextResponse(JSON.stringify(result[0]))
+
 }
